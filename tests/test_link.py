7ca795c511ac1f7d3e294072a01bddb7278be1a3
Edward Prendergast <info@madduck.co.uk>
2012-01-14 17:19:46 +0000

    Modified test

--- tests/test_link.py
+++ tests/test_link.py
@@ -3,7 +3,7 @@ import unittest
 
 class TestSequenceFunctions(unittest.TestCase):
     def setUp(self):
-        self.link = link('http://google.com')
+        self.link = link.link('http://google.com')
 
     def test_is_success(self):
         self.assertEqual(self.link.is_success, 1)
