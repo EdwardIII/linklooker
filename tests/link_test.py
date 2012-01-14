1d84c03c9dc9d87866d759630affab934d04e37d
Edward Prendergast <info@madduck.co.uk>
2012-01-14 17:13:06 +0000

    Added tests, ignore pyc

--- tests/link_test.py
+++ tests/link_test.py
@@ -1,9 +1,9 @@
-import unittest
 from linklooker import link
+import unittest
 
 class TestSequenceFunctions(unittest.TestCase):
     def setUp(self):
-        self.link = linklooker.link('http://google.com')
+        self.link = link('http://google.com')
 
     def test_is_success(self):
         self.assertEqual(self.link.is_success, 1)
