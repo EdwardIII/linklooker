64f29b47758e77bf3393bb2e31fd06196466a296
Edward Prendergast <info@madduck.co.uk>
2012-01-15 16:18:40 +0000

    Removed junk, added console client

--- linklooker.py
+++ linklooker.py
@@ -1,6 +1,14 @@
-from Linklooker.Linklooker import Links, Link
+from linklooker.linklooker import Links, Link
 
-links = Links(["http://google.com"]) 
+links = Links(
+            [
+            "http://google.com",
+            "http://yahoo.com",
+            "http://totally-made-up-domain-name.com",
+            ]
+        ) 
 
-for link in links.status_table():
-    print link
+links_table = links.status_table()
+for link in links_table.keys():
+    status = 'Success' if links_table[link] == True else 'Failure'
+    print format(link).rjust(2) + ':', format(status).rjust(3)
