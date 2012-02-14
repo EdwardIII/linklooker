5ab65767e95a446a99f16bcb8063415c9bb48893
Edward Prendergast <edward@adaodesign.com>
2012-02-14 15:49:02 +0000

    Implemented output as CSV in console tool

--- linklooker.py
+++ linklooker.py
@@ -14,18 +14,49 @@ import argparse
 #    status = 'Success' if links_table[link] == True else 'Failure'
 #    print '{:<40} {}'.format(link + ':', status);
 
+def output_results(results):
+    if not results:
+        exit("Couldn't find any results!")
+
+    import os
+    for row in results:
+        for key in row.keys():
+            print '{} {}'.format(key, row[key])
+        print os.linesep
+    
+    if parser.parse_args().output:
+        #out_f = open(parser.parse_args().output, 'w')
+        out_f = parser.parse_args().output
+
+        # Write out header
+        header = results[0].keys()
+        #out_f.write(header)
+
+        # Write out dictionary
+        import csv
+        print header
+        data = csv.DictWriter(out_f, header,delimiter=',') 
+        data.writeheader()
+        for r in results:
+            data.writerow(r)
+
+        out_f.close()
+
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--csv', help='Provide a CSV to scan links from with the format: link, link to look for')
-
-csv_name = parser.parse_args().csv
+parser.add_argument('--prcsv', help='Provide a CSV to scan links from with the pagerank included in sho\'s format')
+parser.add_argument('--output', type=argparse.FileType('w'), help='Path to output CSV')
 
 links = Links()
-results = links.status_table_from_csv(csv_name)
+if parser.parse_args().csv:
+    print "Parsing CSV..."
+    results = links.status_table_from_csv(parser.parse_args().csv)
+    output_results(results) 
 
-import os
-for row in results:
-    for key in row.keys():
-        print '{} {}'.format(key, row[key])
-    print os.linesep
-
+if  parser.parse_args().prcsv:
+    print "Parsing Pagerank CSV..."
+    results = links.status_table_from_pagerank_csv(parser.parse_args().prcsv)
+    output_results(results)
 
